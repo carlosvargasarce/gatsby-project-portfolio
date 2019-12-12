27daf529f7f53dd68126a85f4f1189f4e9b186b3
woxry <imwoxry@gmail.com>
2019-12-12 07:31:27 -0600

    Test form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -33,7 +33,21 @@ export default ({ data }) => {
 
         {/* Form */}
         <div className={styles.container}>
-          
+          <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+            <input type="hidden" name="bot-field" />
+          </form>
         </div>
       </div>
     </Layout>
